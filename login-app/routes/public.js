afb9f48ccb82200369628710e218f28cd60fc4b7
Eduardo Balbino <valencabalbino@gmail.com>
2025-09-18 22:46:31 -0300

    error

--- login-app/routes/public.js
+++ login-app/routes/public.js
@@ -1,10 +1,31 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
+import { PrismaClient } from '@prisma/client'
 
+const prisma = new PrismaClient()
 const router = express.Router()
 
-router.post('/cadastro', (req, res) => {
-    const user = req.body
-    res.status(201).json(user)
+
+router.post('/cadastro', async (req, res) => {
+    try{ 
+        const user = req.body
+
+        const salt = await bcrypt.genSalt(10)
+        
+        const hashPassword = await bcrypt.hash(user.password, salt)
+
+        const userDB = await prisma.user.create({
+            data: {
+                email: user.email,
+                name: user.name,
+                password: hashPassword,
+            }
+        })
+
+        res.status(201).json(userDB)
+    } catch (err) {
+        res.status(500).json({message: "Internal server error"})
+    }
 })
  
 export default router
--- login-app/server.js
+++ login-app/server.js
@@ -9,4 +9,4 @@ app.use('/', publicRoutes)
 
 app.listen(3000, () => {
   console.log('Servidor rodando em http://localhost:3000')
-});
+})
